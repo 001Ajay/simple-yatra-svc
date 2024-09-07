c9d9da0a7d32d3cf525de07d5a13dc4c779fcdd8
Ajay Das <ajaydas5478@gmail.com>
2024-09-07 19:34:42 +0100

    [backend] Postgres database connection details added and schema sql for 'requester' table added

---
+++ src/main/resources/db-scripts/schema.sql
@@ -0,0 +1,9 @@
+create table requester (
+    requester_id        INT primary key,
+    name                varchar(255),
+    password            varchar(255),
+    email               varchar(255),
+    phone               varchar(255),
+    employee_id         varchar(255),
+    office         varchar(255)
+);
